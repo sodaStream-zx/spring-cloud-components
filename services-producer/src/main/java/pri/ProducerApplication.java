b7b2de2971002a1794bbc04eb9009b7a0f7595ec
riceincoke <42928623+riceincoke@users.noreply.github.com>
2020-04-19 00:00:50 +0800

    环境搭建

---
+++ services-producer/src/main/java/pri/ProducerApplication.java
@@ -0,0 +1,19 @@
+package pri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Twilight
+ * @desc
+ * @createTime 2020-04-18-23:30
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ProducerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerApplication.class, args);
+    }
+
+}
